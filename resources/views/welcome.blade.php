abe1f0b8fa6cb88fec11991f49943cf2f1e59ebe
igor <igor.moreira@pedreira.org>
2019-10-07 19:54:23 -0300

    Configurações iniciais

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.menu')
+
+@section('content')
+<div>
+    dlkjflkfjlkfjlkfd
+</div>
+@endsection
---
+++ webpack.mix.js
@@ -0,0 +1,19 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .copy('node_modules/semantic-ui/dist/semantic.min.css','public/css/semantic.min.css')
+    .copy('node_modules/semantic-ui/dist/semantic.min.js','public/js/semantic.min.js')
+    .copy('node_modules/semantic-ui/dist/themes/basic/assets/fonts/icons.woff','public/css/themes/default/assets/fonts/icons.woff')
+    .copy('node_modules/semantic-ui/dist/themes/basic/assets/fonts/icons.ttf','public/css/themes/default/assets/fonts/icons.ttf');
